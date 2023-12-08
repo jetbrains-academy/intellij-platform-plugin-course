c2e562ce700506c8a880e5721eb9f4c9b6892f7d
Valerii <valerii.ovchinnikov@jetbrains.com>
2023-12-08 11:11:49 +0100

    add annotations done

--- courseSection/psi/addPSI/test/Tests.kt
+++ courseSection/psi/addPSI/test/Tests.kt
@@ -1,5 +1,9 @@
+import com.intellij.ide.highlighter.JavaFileType
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiMethod
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import junit.framework.TestCase
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtNamedFunction
 
@@ -31,5 +35,31 @@ class Test : BasePlatformTestCase() {
             val sorted = methods.sortedBy { it.name }
             assertEquals(sorted, methods)
         }
+    }
+
+    fun testAnnotation() {
+        val javaFile = myFixture.configureByText(
+            JavaFileType.INSTANCE, """
+        class SuperClass {
+            public void testMethod() {}
+        }
+
+        class SubClass extends SuperClass {
+            
+            public void testMethod() {}
+        }
+    """.trimIndent())
+
+        addAnnotations(javaFile)
+        val subClass = PsiTreeUtil.findChildrenOfType(javaFile, PsiClass::class.java)
+            .first { it.name == "SubClass" }
+        val methods = PsiTreeUtil.findChildrenOfType(subClass, PsiMethod::class.java)
+
+        val testMethod = methods.first { it.name == "testMethod" }
+        val hasOverrideAnnotation = testMethod.modifierList.findAnnotation("Override") != null
+
+        // Assert that the @Override annotation is present
+        assertTrue("The method 'testMethod' should have @Override annotation", hasOverrideAnnotation)
+
     }
 }
