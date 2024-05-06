71b3ad338d5ff4fba7ee76a70025a0a9280ab04d
Valerii <valerii.ovchinnikov@jetbrains.com>
2024-05-06 13:21:48 +0200

    consistency tests for programming tasks done

--- psiSection/editingPsiElementsLesson/renameFunctionProgrammingTask/test/org/jetbrains/academy/plugin/course/dev/edit/Tests.kt
+++ psiSection/editingPsiElementsLesson/renameFunctionProgrammingTask/test/org/jetbrains/academy/plugin/course/dev/edit/Tests.kt
@@ -4,28 +4,17 @@ import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import org.jetbrains.kotlin.psi.KtNamedFunction
 
-class Test : BasePlatformTestCase() {
+abstract class BaseEditFunctionNameTest : BasePlatformTestCase() {
+    private fun getResourceFileContent(relativePath: String) =
+        EditFunctionNameTest::class.java.getResourceAsStream(BASE_PATH + relativePath)?.bufferedReader()?.use { it.readText() } ?: ""
+    private fun getFile(fileContent: String) =
+        myFixture.configureByText("MyClass.kt", fileContent) ?: error("Internal course error!")
 
-    private fun String.snakeToCamelCase(): String {
-        return this.split('_').withIndex().joinToString("") { (index, it) ->
-            if (index == 0) it.lowercase() else it.lowercase().replaceFirstChar(Char::titlecase)
-        }
-    }
-
-    // TODO: make parametrized
-    fun testSolution() {
-        val fileContent = """
-            fun say_hello() {
-                println("Hello")
-            }
-            
-            fun say_goodbye() {
-                println("Goodbye")
-            }
-        """.trimIndent()
-        val file = myFixture.configureByText("MyClass.kt", fileContent)
-
+    fun doTest(relativePath: String) {
+        val fileContent = getResourceFileContent(relativePath)
+        val file = getFile(fileContent)
         val functions = PsiTreeUtil.findChildrenOfType(file, KtNamedFunction::class.java)
+
         for (function in functions) {
             val snakeCaseFunctionName = function.name ?: continue
             val expectedCamelCaseFunctionName = snakeCaseFunctionName.snakeToCamelCase()
@@ -39,5 +28,22 @@ class Test : BasePlatformTestCase() {
                 expectedCamelCaseFunctionName == actualCamelCaseFunctionName
             )
         }
+    }
+
+    private fun String.snakeToCamelCase(): String {
+        return this.split('_').withIndex().joinToString("") { (index, it) ->
+            if (index == 0) it.lowercase() else it.lowercase().replaceFirstChar(Char::titlecase)
+        }
+    }
+
+    companion object {
+        private const val BASE_PATH = "/backUpProjects/sort-methods-project/src/main/kotlin/org/jetbrains/academy/plugin/course/dev/project/examples/"
+    }
+}
+
+class EditFunctionNameTest : BaseEditFunctionNameTest() {
+
+    fun testSolution() {
+        doTest("SnakeCaseFunctions.kt")
     }
 }
