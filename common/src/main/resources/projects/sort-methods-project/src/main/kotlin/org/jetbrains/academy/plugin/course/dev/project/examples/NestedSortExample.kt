93387af5e704379087b4b2f4860cd2cd4c13a928
Valerii <valerii.ovchinnikov@jetbrains.com>
2024-05-06 19:11:16 +0200

    project updated

--- common/src/main/resources/projects/sort-methods-project/src/main/kotlin/org/jetbrains/academy/plugin/course/dev/project/examples/NestedSortExample.kt
+++ common/src/main/resources/projects/sort-methods-project/src/main/kotlin/org/jetbrains/academy/plugin/course/dev/project/examples/NestedSortExample.kt
@@ -1,18 +1,31 @@
 package org.jetbrains.academy.plugin.course.dev.project.examples
-class ComplexMethods {
+
+class NestedSortExample {
+
     fun calculate(value: Int): Int {
+
         fun innerFunction(x: Int): Int {
             return x * x
         }
+
+        fun fistInnerFunction(x: Int): Int {
+            return x + x
+        }
+
         return innerFunction(value)
     }
+
+    fun bigCalculate(value: Int): Int {
+        return calculate(value)
+    }
+
     companion object {
         fun staticMethod() {
             println("Static method in companion object")
         }
-    }
-}
 
-fun anotherTopLevelFunction() {
-    println("Another top-level function")
+        fun anotherStaticMethod() {
+            println("Another static method in companion object")
+        }
+    }
 }
---
+++ common/src/main/resources/projects/sort-methods-project/src/main/kotlin/org/jetbrains/academy/plugin/course/dev/project/examples/SnakeCaseFunctions.kt
@@ -0,0 +1,15 @@
+package org.jetbrains.academy.plugin.course.dev.project.examples
+
+fun first_snake_method() {
+    println("First snake method")
+}
+
+fun second_snake_method() {
+    println("Second snake method")
+}
+
+class SnakeCaseFunctions {
+    fun third_snake_method() {
+        println("Third snake method")
+    }
+}
