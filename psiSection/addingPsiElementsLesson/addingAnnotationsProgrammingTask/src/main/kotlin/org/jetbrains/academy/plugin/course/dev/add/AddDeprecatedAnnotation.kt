54669618d21f2cdc59d76279b607738fe7e85576
Valerii <valerii.ovchinnikov@jetbrains.com>
2024-05-14 09:07:00 +0200

    fix lesson by changing name

---
+++ psiSection/addingPsiElementsLesson/addingAnnotationsProgrammingTask/src/main/kotlin/org/jetbrains/academy/plugin/course/dev/add/AddDeprecatedAnnotation.kt
@@ -0,0 +1,33 @@
+package org.jetbrains.academy.plugin.course.dev.add
+
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.kotlin.psi.KtFunction
+import org.jetbrains.kotlin.psi.KtPsiFactory
+
+
+private fun addDeprecatedAnnotation(ktFunction: KtFunction, project: Project) {
+    val factory = KtPsiFactory(project)
+    val deprecatedAnnotation = factory.createAnnotationEntry("@Deprecated(\"This method is deprecated\")")
+    ktFunction.addAnnotationEntry(deprecatedAnnotation)
+}
+
+private fun hasDeprecatedAnnotation(ktFunction: KtFunction): Boolean {
+    return ktFunction.annotationEntries.any {
+        it.shortName?.asString() == "Deprecated"
+    }
+}
+
+fun addDeprecatedAnnotations(psiFile: PsiFile) {
+    val project = psiFile.project
+    WriteCommandAction.runWriteCommandAction(project) {
+        val ktFunctions = PsiTreeUtil.findChildrenOfType(psiFile, KtFunction::class.java)
+        for (ktFunction in ktFunctions) {
+            if (!hasDeprecatedAnnotation(ktFunction)) {
+                addDeprecatedAnnotation(ktFunction, project)
+            }
+        }
+    }
+}
